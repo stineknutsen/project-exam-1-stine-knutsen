d64813f76f610dd5c9aa4fa9e989bca05bc0752d
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2024-11-27 20:51:24 +0100

    moved api.js to shared

---
+++ js/shared/api.js
@@ -0,0 +1,10 @@
+const baseUrl = "https://v2.api.noroff.dev";
+export const apiKey = "ee1394aa-e6c3-4f10-9cac-7a3e0835c62a";
+
+export const registerEndpoint = `${baseUrl}/auth/register`;
+export const loginEndpoint = `${baseUrl}/auth/login`;
+
+const blogName = "hotview_admin";
+export const allPostsEndpoint = `${baseUrl}/blog/posts/${blogName}`;
+export const singlePostEndpoint = (postId) =>
+  `${baseUrl}/blog/posts/${blogName}/${postId}`;
